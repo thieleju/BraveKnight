97267f2cf605657be58c280076192bb6fab1a10a
Julian <thiele.juli@gmail.com>
2022-12-29 01:27:02 +0100

    Fixed skip boss bug

--- Assets/Script/PropsAltar.cs
+++ Assets/Script/PropsAltar.cs
@@ -19,6 +19,8 @@ public class PropsAltar : MonoBehaviour
 
     if (!isEnd) return;
 
+    GameObject[] mobs = GameObject.FindGameObjectsWithTag("Mob");
+    if (mobs.Length != 0) return;
 
     // wait 2 seconds
     StartCoroutine(waiter());
